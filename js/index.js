30a6421fbcfee3f9748425ddf9953d8822707b2d
jarosasan <jaroslavlo@gmail.com>
2019-11-01 17:24:08 +0200

    Back to top button #11

--- js/index.js
+++ js/index.js
@@ -45,3 +45,10 @@ const timer = setInterval(function(){
         }
 
 }, 1000);
+
+
+// Scroll to top
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
